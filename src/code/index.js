e0ed0d0a8abf4cd35c4f45c9d0911072036776c9
Damien <damien.z.hall@gmail.com>
2022-06-20 22:35:46 -0500

    Finalized hopefully

---
+++ src/code/index.js
@@ -0,0 +1,38 @@
+let getScamScore = () => {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      alert(xhr.response);
+      let scamScore = JSON.parse(xhr.response).msg;
+      if (!scamScore.length) {
+          let scamText = "";
+          if (scamScore < 0.45) {
+            scamText = "This is fact!";
+          } else if (scamScore > 0.55) {
+            scamText = "This is fake!";
+          } else {
+            scamText = "I'm not sure!";
+          }
+          document.getElementById("scoreText").innerHTML = scamText;
+      } else {
+        document.getElementById("text").value = JSON.parse(xhr.response).msg;
+      }
+    }
+  }
+  xhr.open("POST", 'http://127.0.0.1:5000/news', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  if (document.getElementById('text').value.substring(0, 7) !== 'http://') {
+    // xhr.send(JSON.stringify({
+    //   text:document.getElementById('text').value
+    // }));
+    xhr.send(JSON.stringify({
+      summary:document.getElementById('text').value
+    }));
+  } else {
+    xhr.send(JSON.stringify({
+      summary:document.getElementById('text').value
+    }));
+  }
+}
+window.onload = () => {
+}
